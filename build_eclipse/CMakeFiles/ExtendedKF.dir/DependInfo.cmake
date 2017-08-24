7075527ed0991f2aa8b5bc3b7406f635d9ea84ed
Inhan Song <cyrano5614@gmail.com>
2017-08-24 06:34:10 -0700

    submission

---
+++ build_eclipse/CMakeFiles/ExtendedKF.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cyrano5614/Dropbox/Documents/Udacity/SDCND/Term2/CarND-Extended-Kalman-Filter-Project/src/FusionEKF.cpp" "/home/cyrano5614/Dropbox/Documents/Udacity/SDCND/Term2/CarND-Extended-Kalman-Filter-Project/build_eclipse/CMakeFiles/ExtendedKF.dir/FusionEKF.cpp.o"
+  "/home/cyrano5614/Dropbox/Documents/Udacity/SDCND/Term2/CarND-Extended-Kalman-Filter-Project/src/kalman_filter.cpp" "/home/cyrano5614/Dropbox/Documents/Udacity/SDCND/Term2/CarND-Extended-Kalman-Filter-Project/build_eclipse/CMakeFiles/ExtendedKF.dir/kalman_filter.cpp.o"
+  "/home/cyrano5614/Dropbox/Documents/Udacity/SDCND/Term2/CarND-Extended-Kalman-Filter-Project/src/main.cpp" "/home/cyrano5614/Dropbox/Documents/Udacity/SDCND/Term2/CarND-Extended-Kalman-Filter-Project/build_eclipse/CMakeFiles/ExtendedKF.dir/main.cpp.o"
+  "/home/cyrano5614/Dropbox/Documents/Udacity/SDCND/Term2/CarND-Extended-Kalman-Filter-Project/src/tools.cpp" "/home/cyrano5614/Dropbox/Documents/Udacity/SDCND/Term2/CarND-Extended-Kalman-Filter-Project/build_eclipse/CMakeFiles/ExtendedKF.dir/tools.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -18,22 +18,57 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  float c_1 = sqrt(px * px + py * py);
+  if (fabs(c_1) < 0.0001)
+  {
+      c_1 = 0.000001;
+  }
+  float c_2 = atan2(py, px);
+
+  float c_3 = (px * vx + py * vy) / c_1;
+
+  VectorXd H_func(3);
+  H_func << c_1, c_2, c_3;
+
+  VectorXd y = z - H_func;
+
+  y[1] = atan2(sin(y[1]), cos(y[1]));
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
